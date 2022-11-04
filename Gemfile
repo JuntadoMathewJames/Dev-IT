97d359590f9d97cf2af98047ea667b3a6f073f1b
JuntadoMathewJames <jmjuntado@gbox.adnu.edu.ph>
2022-11-04 14:36:07 +0800

    changed database from sqlite to mysql

--- Gemfile
+++ Gemfile
@@ -13,8 +13,8 @@ gem "rails", "~> 7.0.4"
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
-# Use sqlite3 as the database for Active Record
-gem "sqlite3", "~> 1.4"
+# Use mysql2 as the database for Active Record
+gem "mysql2", "~> 0.5"
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
@@ -65,7 +65,7 @@ group :development do
   # gem "rack-mini-profiler"
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
-  # gem "spring"
+  #gem "spring"
 end
 
 group :test do
--- db/schema.rb
+++ db/schema.rb
@@ -11,73 +11,73 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_26_094346) do
-  create_table "expenses", force: :cascade do |t|
+  create_table "expenses", charset: "utf8mb4", force: :cascade do |t|
     t.text "description"
-    t.decimal "amount"
+    t.decimal "amount", precision: 10
     t.date "dateOfExpense"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "orders", force: :cascade do |t|
+  create_table "orders", charset: "utf8mb4", force: :cascade do |t|
     t.integer "transaction_id"
     t.integer "product_id"
-    t.decimal "quantity"
-    t.decimal "price"
+    t.decimal "quantity", precision: 10
+    t.decimal "price", precision: 10
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "payments", force: :cascade do |t|
+  create_table "payments", charset: "utf8mb4", force: :cascade do |t|
     t.date "dateOfPayment"
-    t.decimal "amount"
+    t.decimal "amount", precision: 10
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "point_of_sales", force: :cascade do |t|
+  create_table "point_of_sales", charset: "utf8mb4", force: :cascade do |t|
     t.integer "subscription_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "products", force: :cascade do |t|
+  create_table "products", charset: "utf8mb4", force: :cascade do |t|
     t.integer "pos_id"
     t.string "productName"
-    t.decimal "quantity"
-    t.decimal "pricePerUnit"
+    t.decimal "quantity", precision: 10
+    t.decimal "pricePerUnit", precision: 10
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "sales", force: :cascade do |t|
+  create_table "sales", charset: "utf8mb4", force: :cascade do |t|
     t.integer "pos_id"
     t.date "dateOfSales"
-    t.decimal "netSales"
-    t.decimal "grossSales"
-    t.decimal "beginningBalance"
+    t.decimal "netSales", precision: 10
+    t.decimal "grossSales", precision: 10
+    t.decimal "beginningBalance", precision: 10
     t.string "remarks"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "subscriptions", force: :cascade do |t|
+  create_table "subscriptions", charset: "utf8mb4", force: :cascade do |t|
     t.integer "user_id"
-    t.decimal "payment"
+    t.decimal "payment", precision: 10
     t.date "dateOfPayment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "transactions", force: :cascade do |t|
+  create_table "transactions", charset: "utf8mb4", force: :cascade do |t|
     t.integer "pos_id"
     t.date "dateOfTransaction"
-    t.decimal "totalPrice"
+    t.decimal "totalPrice", precision: 10
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
     t.string "status"
